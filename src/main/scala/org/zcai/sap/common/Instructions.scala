ff799a032fb91f2f75465b6532ed4bbcf8ef2f06
Zixian Cai <2891235+caizixian@users.noreply.github.com>
2022-04-08 14:19:10 +1000

    Add SAP-1 instructions

---
+++ src/main/scala/org/zcai/sap/common/Instructions.scala
@@ -0,0 +1,11 @@
+package org.zcai.sap.common
+
+import chisel3.util._
+
+object Instructions {
+  def LDA: BitPat = BitPat("b0000????")
+  def ADD: BitPat = BitPat("b0001????")
+  def SUB: BitPat = BitPat("b0010????")
+  def OUT: BitPat = BitPat("b1110????")
+  def HLT: BitPat = BitPat("b1111????")
+}
